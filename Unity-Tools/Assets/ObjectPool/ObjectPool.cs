3ca892f6a19617fed294bfdd014404929fa33721
Malakahh <michael_fuglsang@hotmail.com>
2015-06-14 18:28:04 +0200

    Object Pool 1

---
+++ Unity-Tools/Assets/ObjectPool/ObjectPool.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour {
+    public enum ObjectPoolErrorLevel {LogError, Exceptions}
+
+    public static ObjectPoolErrorLevel ErrorLevel = ObjectPoolErrorLevel.LogError;
+    static Dictionary<System.Type, BaseMetaEntry> pools = new Dictionary<System.Type, BaseMetaEntry>();
+
+    /// <summary>
+    /// Releases an object back into the pool.
+    /// </summary>
+    /// <typeparam name="T">Object type to release</typeparam>
+    /// <param name="obj">Object to release</param>
+    public static void Release<T>(T obj)
+    {
+        System.Type t = typeof(T);
+
+        if (pools.ContainsKey(t))
+        {
+            MetaEntry<T> entry = (MetaEntry<T>)pools[t];
+            entry.Pool.Enqueue(obj);
+        }
+        else
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.OBJECT_TYPE_NOT_FOUND);
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.OBJECT_TYPE_NOT_FOUND, t);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Acquires an object from the object pool
+    /// </summary>
+    /// <typeparam name="T">Type of object to acquire</typeparam>
+    /// <returns>Acquired object</returns>
+    public static T Acquire<T>() where T : new()
+    {
+        System.Type t = typeof(T);
+
+        MetaEntry<T> entry;
+
+        if (!pools.ContainsKey(t))
+        {
+            entry = new MetaEntry<T>();
+
+            if (t.IsSubclassOf(typeof(UnityEngine.Object)))
+            {
+                //entry.Pool.Enqueue(AcquireUnityObject<T>());
+            }
+            else
+            {
+                entry.Pool.Enqueue(new T());
+            }
+
+            entry.InstaceCountTotal = 1;
+            pools.Add(t, entry);
+        }
+        else
+        {
+            entry = (MetaEntry<T>)pools[t];
+        }
+        
+        //Below threshold, make more instances
+        if (entry.Pool.Count <= entry.LowerThreshold)
+        {
+            //Double the number of entries
+            for (int i = 0; i < entry.InstaceCountTotal; i++)
+            {
+                entry.Pool.Enqueue(new T());
+            }
+            entry.InstaceCountTotal *= 2;
+        }
+
+        return entry.Pool.Dequeue();
+    }
+
+    /// <summary>
+    /// Instatiates a unity GameObject
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    private static T AcquireUnityObject<T>() where T : UnityEngine.Object
+    {
+        Object[] arr = Resources.LoadAll<T>("");
+        System.Type t = typeof(T);
+
+        //Error if we didn't find anything
+        if (arr == null)
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.RESOURCE_NOT_FOUND);
+                return null;
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.RESOURCE_NOT_FOUND, t);
+            }
+        }
+
+        //Error if we found too much
+        if (arr.Length > 1)
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.OBJECT_TYPE_MUST_BE_UNIQUE);
+                return null;
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.OBJECT_TYPE_MUST_BE_UNIQUE, t);
+            }
+        }
+
+        return (T)arr[0];
+    }
+    
+    /// <summary>
+    /// Sets the lower threshold for when instantiation of new objects should begin. Defaults to 1. If things are spawned often, i.e. bullets, you want to set this higher.
+    /// </summary>
+    /// <typeparam name="T">Object type to set threshold for</typeparam>
+    /// <param name="threshold">The new lower threshold</param>
+    public static void SetLowerInstantiationThreshold<T>(int threshold)
+    {
+        System.Type t = typeof(T);
+
+        if (pools.ContainsKey(t))
+        {
+            MetaEntry<T> entry = (MetaEntry<T>)pools[t];
+            entry.LowerThreshold = threshold;
+        }
+        else
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.OBJECT_TYPE_NOT_FOUND);
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.OBJECT_TYPE_NOT_FOUND, t);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets the lower threshold for when instatiation of new objects should begin. Defaults to 1.
+    /// </summary>
+    /// <typeparam name="T">Object type to get threshold for</typeparam>
+    /// <returns>The lower threshold, or -1 on fail</returns>
+    public static int GetLowerInstatiationThreshold<T>()
+    {
+        System.Type t = typeof(T);
+
+        if (pools.ContainsKey(t))
+        {
+            MetaEntry<T> entry = (MetaEntry<T>)pools[t];
+            return entry.LowerThreshold;
+        }
+        else
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.OBJECT_TYPE_NOT_FOUND);
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.OBJECT_TYPE_NOT_FOUND, t);
+            }
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Gets the total instance count of objects of a given type that exists, both in and out of the pool.
+    /// </summary>
+    /// <typeparam name="T">Object type</typeparam>
+    /// <returns>Instance count</returns>
+    public static int GetInstanceCountTotal<T>()
+    {
+        System.Type t = typeof(T);
+
+        if (pools.ContainsKey(t))
+        {
+            MetaEntry<T> entry = (MetaEntry<T>)pools[t];
+            return entry.InstaceCountTotal;
+        }
+        else
+        {
+            if (ErrorLevel == ObjectPoolErrorLevel.LogError)
+            {
+                Debug.LogError(ErrorStrings.OBJECT_TYPE_NOT_FOUND);
+            }
+            else
+            {
+                throw new ObjectPoolException(ErrorStrings.OBJECT_TYPE_NOT_FOUND, t);
+            }
+        }
+
+        return -1;
+    }
+
+    abstract class BaseMetaEntry {};
+    class MetaEntry<T> : BaseMetaEntry
+    {
+        public Queue<T> Pool = new Queue<T>();
+        public int InstaceCountTotal = 0;
+        public int LowerThreshold = 1;
+    }
+
+    public class ObjectPoolException : System.Exception
+    {
+        public System.Type TypeUsed;
+
+        public ObjectPoolException(string msg, System.Type t) : base(msg)
+        {
+            TypeUsed = t;
+        }
+    }
+
+    private static class ErrorStrings
+    {
+        public static string OBJECT_TYPE_NOT_FOUND = "Object Pool ERROR: Object type not in pool.";
+        public static string OBJECT_TYPE_MUST_BE_UNIQUE = "Object Pool ERROR: Object type must be unique.";
+        public static string RESOURCE_NOT_FOUND = "Object Pool ERROR: Resource not found.";
+    }
+}
---
+++ Unity-Tools/Assets/ObjectPool/Tests/ObjectPoolUnitTest.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPoolUnitTest : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        #if UNITY_EDITOR
+        ObjectPool.ErrorLevel = ObjectPool.ObjectPoolErrorLevel.Exceptions;
+        Debug.Log("*** Running Object Pool Unit Tests...");
+        bool defaultConstructorObject = DefaultConstructorObject();
+        bool defaultConstructorObjectMany = DefaultConstructorObjectMany();
+        bool thresholdDefaultSuccess1 = ThresholdDefaultSuccess1();
+        bool thresholdDefaultSuccess2 = ThresholdDefaultSuccess2();
+        bool thresholdIncreased1 = ThresholdIncreased1();
+        bool thresholdIncreased2 = ThresholdIncreased2();
+        bool release1 = Release1();
+        bool release2 = Release2();
+
+        bool res = defaultConstructorObject && 
+            defaultConstructorObjectMany &&
+            thresholdDefaultSuccess1 &&
+            thresholdDefaultSuccess2 &&
+            thresholdIncreased1 &&
+            thresholdIncreased2 &&
+            release1 &&
+            release2;
+        Debug.Log("*** Test completed with result: " + res);
+        #endif
+	}
+
+    bool DefaultConstructorObject()
+    {
+        TestDataClass data = ObjectPool.Acquire<TestDataClass>();
+        TestDataClass expected = new TestDataClass(1);
+
+        bool res = expected.CheckEqual(data);
+
+        Debug.Log("DefaultConstructorObject: " + res);
+        return res;
+    }
+
+    bool DefaultConstructorObjectMany()
+    {
+        List<TestDataClass> list = new List<TestDataClass>();
+        TestDataClass expected = new TestDataClass(1);
+        int count = 20;
+        for (int i = 0; i < count; i++)
+        {
+            list.Add(ObjectPool.Acquire<TestDataClass>());
+        }
+
+        bool res = true;
+
+        foreach (TestDataClass d in list)
+        {
+            if (!expected.CheckEqual(d))
+                res = false;
+        }
+
+        Debug.Log("DefaultConstructorObjectMany: " + res);
+        return res;
+    }
+
+    bool ThresholdDefaultSuccess1()
+    {
+        int expected = 32;
+        for (int i = 0; i < 31; i++)
+        {
+            ObjectPool.Acquire<ThresholdDerivativeDefaultSuccess1>();
+        }
+        bool res = ObjectPool.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess1>() == expected;
+        Debug.Log("ThresholdDefaultSuccess1: " + res);
+        return res;
+    }
+
+    bool ThresholdDefaultSuccess2()
+    {
+        int expected = 64;
+        for (int i = 0; i < 32; i++)
+        {
+            ObjectPool.Acquire<ThresholdDerivativeDefaultSuccess2>();
+        }
+        bool res = ObjectPool.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess2>() == expected;
+        Debug.Log("ThresholdDefaultSuccess2: " + res);
+        return res;
+    }
+
+    bool ThresholdIncreased1()
+    {
+        int expected = 16;
+        ObjectPool.Acquire<ThresholdDerivativeIncreased1>();
+        ObjectPool.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased1>(2);
+
+        for (int i = 0; i < 13; i++)
+        {
+            ObjectPool.Acquire<ThresholdDerivativeIncreased1>();
+        }
+
+        bool res = ObjectPool.GetInstanceCountTotal<ThresholdDerivativeIncreased1>() == expected;
+        Debug.Log("ThresholdIncreased1: " + res);
+        return res;
+    }
+
+    bool ThresholdIncreased2()
+    {
+        int expected = 32;
+        ObjectPool.Acquire<ThresholdDerivativeIncreased2>();
+        ObjectPool.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased2>(2);
+
+        for (int i = 0; i < 14; i++)
+        {
+            ObjectPool.Acquire<ThresholdDerivativeIncreased2>();
+        }
+        bool res = ObjectPool.GetInstanceCountTotal<ThresholdDerivativeIncreased2>() == expected;
+        Debug.Log("ThresholdIncreased2: " + res);
+        return res;
+    }
+
+    bool Release1()
+    {
+        int expected = 16;
+        ReleaseDerivative1 obj = ObjectPool.Acquire<ReleaseDerivative1>();
+        ObjectPool.SetLowerInstantiationThreshold<ReleaseDerivative1>(2);
+
+        for (int i = 0; i < 13; i++)
+        {
+            ObjectPool.Acquire<ReleaseDerivative1>();
+        }
+
+        ObjectPool.Release<ReleaseDerivative1>(obj);
+        ObjectPool.Acquire<ReleaseDerivative1>();
+
+        bool res = ObjectPool.GetInstanceCountTotal<ReleaseDerivative1>() == expected;
+        Debug.Log("Release1: " + res);
+        return res;
+    }
+
+    bool Release2()
+    {
+        int expected = 16;
+        ReleaseDerivative1 obj = ObjectPool.Acquire<ReleaseDerivative1>();
+        ObjectPool.SetLowerInstantiationThreshold<ReleaseDerivative1>(2);
+
+        for (int i = 0; i < 14; i++)
+        {
+            ObjectPool.Acquire<ReleaseDerivative1>();
+        }
+
+        ObjectPool.Release<ReleaseDerivative1>(obj);
+        ObjectPool.Acquire<ReleaseDerivative1>();
+
+        bool res = ObjectPool.GetInstanceCountTotal<ReleaseDerivative1>() != expected;
+        Debug.Log("Release2: " + res);
+        return res;
+    }
+
+    class TestDataClass
+    {
+        public int myInt;
+        public string myString;
+        public List<string> list = new List<string>();
+        public Vector3 myVector;
+
+        public TestDataClass() : this(1)
+        { }
+
+        public TestDataClass(int num)
+        {
+            myInt = num;
+            myString = "Num: " + num.ToString();
+            for (int i = 0; i < myInt; i++)
+            {
+                list.Add(" entry: " + i);
+            }
+            myVector = new Vector3(myInt, -myInt, myInt);
+        }
+
+        public bool CheckEqual(TestDataClass data)
+        {
+            bool res = true;
+
+            if (data.myInt != this.myInt) res = false;
+            if (data.myString != this.myString) res = false;
+            if (data.myVector != this.myVector) res = false;
+
+            for (int i = 0; i < data.list.Count; i++) if (data.list[i] != this.list[i]) res = false;
+
+            return res;
+        }
+    }
+    class ThresholdDerivativeDefaultSuccess1 : TestDataClass { }
+    class ThresholdDerivativeDefaultSuccess2 : TestDataClass { }
+    class ThresholdDerivativeIncreased1 : TestDataClass { }
+    class ThresholdDerivativeIncreased2 : TestDataClass { }
+    class ReleaseDerivative1 : TestDataClass { }
+    class ReleaseDerivative2 : TestDataClass { }
+}
